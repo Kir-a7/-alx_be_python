9235c5dc686cf9bf87dbf666157664280180dd35
Kira <kirubelteshome6202@gmail.com>
2024-09-25 17:06:52 +0300

    First task

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,5 @@
 
 
-def main():
-    print("Arithmetic Operations")
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
-
-    result = perform_operation(num1, num2, operation)
-    print(f"Result: {result}")
-
 def perform_operation(num1, num2, operation):
     if operation == "subtract":
         return num1-num2   
@@ -19,13 +10,13 @@ def perform_operation(num1, num2, operation):
     else:
         if num2 != 0:
             return num1/num2
+        elif num2 == 0:
+            return print("You can't divde by zero")
         else :
             return print("Invalid inpute")
 
 
 
-if __name__ == "__main__":
-    main()
 
 
 
