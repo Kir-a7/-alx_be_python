95af0a9ad807c77bacd4d6140ddfa0aaf85750b5
Kira <kirubelteshome6202@gmail.com>
2024-10-04 21:12:42 +0300

    Task-2

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,7 +1,10 @@
 def safe_divide(numerator, denominator):
     try:
-        division = numerator/denominator
-        return division
+        num = float(numerator)
+        denom = float(denominator)
+        
+        result = num / denom
+        return f"The result of the division is {result}"
     except ZeroDivisionError as e:
         print(e)
     except ValueError as e :
